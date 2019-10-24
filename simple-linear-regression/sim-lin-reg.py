97e8e341505081476a5ed7c8b7244a0a7a01914a
zeitunik <osipovakarine@gmail.com>
2019-10-24 16:55:23 +0200

    added simple linear regression

--- simple-linear-regression/sim-lin-reg.py
+++ simple-linear-regression/sim-lin-reg.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas
+from sklearn import linear_model
+from sklearn import metrics
 import pylab
 
 df = pandas.read_csv("FuelConsumption.csv")
@@ -36,4 +38,37 @@ ax[2].set_xlabel("CYLINDERS")
 ax[2].set_ylabel("Emission")
 
 plt.tight_layout()
-plt.show()
+# plt.show()
+
+# Creating training 80% and test sets 20%
+mask = np.random.rand(len(df)) < 0.8
+train = cdf[mask]
+test = cdf[~mask]
+
+# sklearn to model the simple linear regression
+print("Doing linear regression between ENGINESIZE and CO2EMMISIONS...")
+regr = linear_model.LinearRegression()
+
+train_x = np.asanyarray(train[['ENGINESIZE']])
+train_y = np.asanyarray(train[['CO2EMISSIONS']])
+regr.fit(train_x, train_y)
+# print the coefficients
+print('Coefficients: ', regr.coef_)
+print('Intercept: ', regr.intercept_)
+
+# plot learned line on the scatter plot
+ax[1].plot(train_x, regr.intercept_[0] + train_x * regr.coef_[0][0], '-k')
+# plt.show()
+
+# Evaluation
+test_x = np.asanyarray(test[['ENGINESIZE']])
+test_y = np.asanyarray(test[['CO2EMISSIONS']])
+test_y_hat = regr.predict(test_x)
+
+mae = np.mean(np.abs(test_y - test_y_hat))
+mse = np.mean((test_y - test_y_hat)**2)
+r2_score = metrics.r2_score(test_y_hat, test_y)
+
+print("Mean Absolute Error: {:.2f}".format(mae))
+print("Mean Square Error: {:.2f}".format(mse))
+print("R2-score: {:.2f}".format(r2_score))
