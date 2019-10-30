7f369a00e3d21327b6ed2b0e71f6e993faed3e8e
zeitunik <osipovakarine@gmail.com>
2019-10-30 17:38:49 +0100

    added functions that split data to train/test, fit and predict

--- multiple-linear-regression/mult-lin-reg.py
+++ multiple-linear-regression/mult-lin-reg.py
@@ -16,3 +16,46 @@ def scatter_plot(cdf, x, y, col='green'):
     plt.xlabel(x)
     plt.ylabel(y)
     plt.show()
+
+
+def split_train_test(cdf, ratio):
+    mask = np.random.rand(len(cdf)) < ratio
+    train = cdf[mask]
+    test = cdf[~mask]
+    return train, test
+
+
+def train_model(train, fuel):
+    regr = linear_model.LinearRegression()
+    vars = [['ENGINESIZE', 'CYLINDERS'], fuel]
+    vars_flat = [item for sublist in vars for item in sublist]
+    print(vars_flat)
+    x = np.asanyarray(train[vars_flat])
+    y = np.asanyarray(train[['CO2EMISSIONS']])
+    regr.fit(x, y)
+    print(f"Intercept: {regr.intercept_}")
+    print(f"Coefficients: {regr.coef_}")
+    return regr
+
+
+def predict(regr, test, fuel):
+    vars = [['ENGINESIZE', 'CYLINDERS'], fuel]
+    vars_flat = [item for sublist in vars for item in sublist]
+    x = np.asanyarray(test[vars_flat])
+    y = np.asanyarray(test[['CO2EMISSIONS']])
+    y_hat = regr.predict(x)
+
+    print(f"Mean Squared Error for {fuel}: {np.mean((y-y_hat)**2)}")
+    print(f"Explained Variance for {fuel}: {regr.score(x, y)}")
+
+
+if __name__ == '__main__':
+    cdf = read_data("../simple-linear-regression/FuelConsumption.csv")
+    scatter_plot(cdf, "ENGINESIZE", "CO2EMISSIONS")
+    train, test = split_train_test(cdf, 0.8)
+
+    fuels = [['FUELCONSUMPTION_COMB'], ['FUELCONSUMPTION_CITY', 'FUELCONSUMPTION_HWY']]
+    for fuel in fuels:
+        model = train_model(train, fuel)
+        predict(model, test, fuel)
+        print("\n")
