801e7bec02c2ecfb3a311d4bf1b0a79058f22570
zeitunik <osipovakarine@gmail.com>
2019-10-30 17:37:30 +0100

    added function that reads data into a dataframe and plots a scatterplot

--- multiple-linear-regression/mult-lin-reg.py
+++ multiple-linear-regression/mult-lin-reg.py
@@ -4,3 +4,15 @@ import pandas
 from sklearn import linear_model
 
 
+def read_data(filename):
+    df = pandas.read_csv(filename)
+    cdf = df[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_CITY', 'FUELCONSUMPTION_HWY', 'FUELCONSUMPTION_COMB',
+              'CO2EMISSIONS']]
+    return cdf
+
+
+def scatter_plot(cdf, x, y, col='green'):
+    plt.scatter(cdf[[x]], cdf[[y]], color=col)
+    plt.xlabel(x)
+    plt.ylabel(y)
+    plt.show()
