b95ed3c6e77ceb8b7dc147f99bc39a701bafc0ef
zeitunik <osipovakarine@gmail.com>
2019-10-31 09:59:46 +0100

    added plotting function, regression model, prediction function

--- polynomial-regression/poly-reg.py
+++ polynomial-regression/poly-reg.py
@@ -3,12 +3,26 @@ import matplotlib.pyplot as plt
 import pandas
 from sklearn import linear_model
 from sklearn.preprocessing import PolynomialFeatures
+from sklearn import metrics
 
 
 def read_data(filename):
     df = pandas.read_csv(filename)
     cdf = df[['ENGINESIZE', 'CYLINDERS', 'FUELCONSUMPTION_COMB', 'CO2EMISSIONS']]
     return cdf
+
+
+def scatter_plot(x, y, regr, deg=2, col='green'):
+    plt.scatter(x, y, color=col)
+    plt.xlabel("Engine size")
+    plt.ylabel("CO2 Emission")
+
+    xx = np.arange(0, 10, 0.01)
+    yy = regr.intercept_[0] + regr.coef_[0][1] * xx + regr.coef_[0][2] * xx ** 2
+    if deg == 3:
+        yy += regr.coef_[0][3] * xx**3
+    plt.plot(xx, yy, 'r')
+    plt.show()
 
 
 def split_train_test(cdf, ratio):
@@ -18,11 +32,47 @@ def split_train_test(cdf, ratio):
     return train, test
 
 
-def model(train):
+def train_model(train, deg=2):
     x = np.asanyarray(train[['ENGINESIZE']])
     y = np.asanyarray(train[['CO2EMISSIONS']])
 
-    poly = PolynomialFeatures(2)
+    poly = PolynomialFeatures(deg)
     poly_x = poly.fit_transform(x)
+
+    regr = linear_model.LinearRegression()
+    regr.fit(poly_x, y)
+
+    print(f"Intercept: {regr.intercept_}")
+    print(f"Coefficients: {regr.coef_}")
+
+    scatter_plot(x, y, regr, deg)
+
+    return regr
+
+
+def predict(test, regr, deg=2):
+    x = np.asanyarray(test[['ENGINESIZE']])
+    y = np.asanyarray(test[['CO2EMISSIONS']])
+
+    poly = PolynomialFeatures(deg)
+    poly_x = poly.fit_transform(x)
+    y_hat = regr.predict(poly_x)
+
+    print(f"Mean Absolute Error: {np.mean(np.abs(y - y_hat))}")
+    print(f"Mean Squared Error: {np.mean((y - y_hat)**2)}")
+    print(f"R2-score: {metrics.r2_score(y, y_hat)}")
+
+
+if __name__ == '__main__':
+    cdf = read_data("../simple-linear-regression/FuelConsumption.csv")
+    train, test = split_train_test(cdf, 0.8)
+
+    degree = [2, 3]
+    for deg in degree:
+        print(f"Polynomial degree {deg}")
+        model = train_model(train, deg)
+        predict(test, model, deg)
+
+
 
 
