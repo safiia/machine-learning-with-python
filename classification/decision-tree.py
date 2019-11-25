31e34a570ed28d42ce56ca6b0271180cb5f08a00
zeitunik <osipovakarine@gmail.com>
2019-11-25 18:44:07 +0100

    added accuracy calculation and visualizing the tree

--- classification/decision-tree.py
+++ classification/decision-tree.py
@@ -1,8 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import pandas
 from sklearn.tree import DecisionTreeClassifier
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn import metrics
+from sklearn.externals.six import StringIO
+import pydotplus
+import matplotlib.image as mpimg
+from sklearn import tree
 
 
 def read_data(filename):
@@ -19,7 +25,7 @@ def read_data(filename):
     data = data[cols].values
     x = data[:, :-1]
     y = data[:, -1]
-    return x, y
+    return x, y, cols
 
 
 def preprocess_data(col, values):
@@ -61,8 +67,28 @@ def predict(model, x_test):
     return y_hat
 
 
+def accuracy(y_hat, y_test):
+    acc = metrics.accuracy_score(y_test, y_hat)
+    # acc = np.sum(y_hat==y_test) / y_test.shape[0]
+    return acc
+
+
+def visualize_tree(model, y, y_train, cols):
+    dot_data = StringIO()
+    filename = "drug_tree.png"
+    feature_names = cols[0:5]
+    target_names = np.unique(y).tolist()
+    out = tree.export_graphviz(model, feature_names=feature_names, out_file=dot_data,
+                               class_names=np.unique(y_train), filled=True, special_characters=True, rotate=False)
+    graph = pydotplus.graph_from_dot_data(dot_data.getvalue())
+    graph.write_png(filename)
+    img = mpimg.imread(filename)
+    plt.figure(figsize=(100, 200))
+    plt.imshow(img, interpolation='nearest')
+
+
 if __name__ == '__main__':
-    x, y = read_data("drug200.csv")
+    x, y, cols = read_data("drug200.csv")
     x[:, 1] = preprocess_data(x[:, 1], ['F', 'M'])
     x[:, 2] = preprocess_data(x[:, 2], ['LOW', 'NORMAL', 'HIGH'])
     x[:, 3] = preprocess_data(x[:, 3], ['NORMAL', 'HIGH'])
@@ -70,6 +96,14 @@ if __name__ == '__main__':
     x_train, x_test, y_train, y_test = split_data(x, y, 0.7)
 
     model = train(x_train, y_train)
+    y_hat = predict(model, x_test)
+
+    print(f"Model accuracy: {accuracy(y_hat, y_test)}")
+
+    visualize_tree(model, y, y_train, cols)
+
+
+
 
 
 
