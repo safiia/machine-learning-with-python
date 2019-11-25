5bc39d8ecfac98dbaa0485adca46c59b792adfa7
zeitunik <osipovakarine@gmail.com>
2019-11-25 13:49:23 +0100

    added training and prediction functions

--- classification/knn.py
+++ classification/knn.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import pandas
 from sklearn import preprocessing
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
 
 def read_data(file_name):
@@ -47,7 +48,29 @@ def split_data(x, y, train_size):
     return x_train, x_test, y_train, y_test
 
 
+def train(x_train, y_train, k=4):
+    """
+    Train the knn classifier on the training set using k neighbors. This is done by building a KNeighborsClassifier
+    that fits the training data and the target values.
+    :param x_train:
+    :param y_train:
+    :param k: number of neighbors to consider
+    :return:
+    """
+    neighbors = KNeighborsClassifier(n_neighbors=k).fit(x_train, y_train)
+    return neighbors
+
+
+def predict(neighbors, x_test):
+    y_hat = neighbors.predict(x_test)
+    return y_hat
+
+
 if __name__ == '__main__':
     data = read_data("teleCust1000t.csv")
     norm_data = normalize_data(data)
     x_train, x_test, y_train, y_test = split_data(norm_data[0], norm_data[1], 0.8)
+
+    model4 = train(x_train, y_train)
+    y_hat = predict(model4, x_test)
+
