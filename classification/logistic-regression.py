2079e9e5408b966b384a3df1b40caa8d927e191e
zeitunik <osipovakarine@gmail.com>
2019-12-01 15:21:06 +0100

    initial commit

---
+++ classification/logistic-regression.py
@@ -0,0 +1,13 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import pandas
+import sklearn
+
+
+def read_data(file_name):
+    pass
+
+
+if __name__ == '__main__':
+    read_data(".csv")
